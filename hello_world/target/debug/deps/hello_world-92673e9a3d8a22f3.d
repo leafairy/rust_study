bb2f650f33a58dfc8a47e0118f543bfcd1224d15
leafairy <liujian9696@outlook.com>
2023-02-26 10:29:31 +0800

    deleted:    .gitignore 	renamed:    Cargo.lock -> hello_world/Cargo.lock 	renamed:    Cargo.toml -> hello_world/Cargo.toml 	renamed:    src/main.rs -> hello_world/src/main.rs 	new file:   hello_world/target/.rustc_info.json 	new file:   hello_world/target/CACHEDIR.TAG 	new file:   hello_world/target/debug/.cargo-lock 	new file:   hello_world/target/debug/.fingerprint/hello_world-92673e9a3d8a22f3/bin-hello_world 	new file:   hello_world/target/debug/.fingerprint/hello_world-92673e9a3d8a22f3/bin-hello_world.json 	new file:   hello_world/target/debug/.fingerprint/hello_world-92673e9a3d8a22f3/dep-bin-hello_world 	new file:   hello_world/target/debug/.fingerprint/hello_world-92673e9a3d8a22f3/invoked.timestamp 	new file:   hello_world/target/debug/.fingerprint/hello_world-cee2712553938527/dep-test-bin-hello_world 	new file:   hello_world/target/debug/.fingerprint/hello_world-cee2712553938527/invoked.timestamp 	new file:   hello_world/target/debug/.fingerprint/hello_world-cee2712553938527/test-bin-hello_world 	new file:   hello_world/target/debug/.fingerprint/hello_world-cee2712553938527/test-bin-hello_world.json 	new file:   hello_world/target/debug/deps/hello_world-92673e9a3d8a22f3.d 	new file:   hello_world/target/debug/deps/hello_world-cee2712553938527.d 	new file:   hello_world/target/debug/deps/libhello_world-92673e9a3d8a22f3.rmeta 	new file:   hello_world/target/debug/deps/libhello_world-cee2712553938527.rmeta 	new file:   hello_world/target/debug/incremental/hello_world-3d82c9f1weyos/s-gikx2lruq9-dj1ykd-3ldx6ins42iy5/dep-graph.bin 	new file:   hello_world/target/debug/incremental/hello_world-3d82c9f1weyos/s-gikx2lruq9-dj1ykd-3ldx6ins42iy5/query-cache.bin 	new file:   hello_world/target/debug/incremental/hello_world-3d82c9f1weyos/s-gikx2lruq9-dj1ykd-3ldx6ins42iy5/work-products.bin 	new file:   hello_world/target/debug/incremental/hello_world-3d82c9f1weyos/s-gikx2lruq9-dj1ykd.lock 	new file:   hello_world/target/debug/incremental/hello_world-rpk2rvj5szbu/s-gikx2lrur5-r2o1we-3ihswp68bdlvg/dep-graph.bin 	new file:   hello_world/target/debug/incremental/hello_world-rpk2rvj5szbu/s-gikx2lrur5-r2o1we-3ihswp68bdlvg/query-cache.bin 	new file:   hello_world/target/debug/incremental/hello_world-rpk2rvj5szbu/s-gikx2lrur5-r2o1we-3ihswp68bdlvg/work-products.bin 	new file:   hello_world/target/debug/incremental/hello_world-rpk2rvj5szbu/s-gikx2lrur5-r2o1we.lock

---
+++ hello_world/target/debug/deps/hello_world-92673e9a3d8a22f3.d
@@ -0,0 +1,5 @@
+c:\Users\yabai\Documents\rust_study\hello_world\target\debug\deps\hello_world-92673e9a3d8a22f3.rmeta: src\main.rs
+
+c:\Users\yabai\Documents\rust_study\hello_world\target\debug\deps\hello_world-92673e9a3d8a22f3.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/deps/hello_world-cee2712553938527.d
@@ -0,0 +1,5 @@
+c:\Users\yabai\Documents\rust_study\hello_world\target\debug\deps\hello_world-cee2712553938527.rmeta: src\main.rs
+
+c:\Users\yabai\Documents\rust_study\hello_world\target\debug\deps\hello_world-cee2712553938527.d: src\main.rs
+
+src\main.rs:
